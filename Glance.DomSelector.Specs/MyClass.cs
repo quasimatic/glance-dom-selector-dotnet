73076e088666dea492bc6b39f9f667c1dc1610c1
Cory Wheeler <corywheeler@gmail.com>
2016-12-28 00:22:05 -0500

    An initial ChromeDriver test to load a page.
    The unfortunate piece of this is it looks like there is no way to override
    in the .NET Selenium Webdriver the name of the executable for the Chrome
    Driver.
    
    The constructor for ChromeDriver takes a path to the directory that the
    "chromedriver" executable lives in. Giving this the path to where my
    executable lived did not work. This is because my executable was loaded
    by the selenium-standalone npm package, and the chromedriver executable is
    named "2.25-x64-chromedriver".
    
    https://www.npmjs.com/package/selenium-standalone
    
    I was not under the influence of wanting to change this. For me it lives at
    /usr/local/lib/node_modules/selenium-standalone/.selenium/chromedriver/
    
    and as it was in a git repo, I didn't feel it wise to rename.
    
    So, what does one do?
    
    I copied it to the directory
    
    /Users/corywheeler/Documents/projects/chromestuff/
    
    So that the full path is now
    
    /Users/corywheeler/Documents/projects/chromestuff/chromedriver
    
    where "chromedriver" is a copy of "2.25-x64-chromedriver".
    
    I have yet to find a way to be able to set the name of the chrome driver
    executable name, which is unfortunate. Perhaps there is one, I've just
    not stumbled onto it yet.

---
+++ Glance.DomSelector.Specs/MyClass.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+
+namespace Glance.DomSelector.Specs
+{
+	[TestFixture]
+	public class When_trying_to_see_if_it_runs
+	{
+		[Test]
+		public void lets_scrape_something()
+		{
+			ChromeOptions options = new ChromeOptions();
+			options.AddArguments("start-maximized");
+
+			using (var driver = new ChromeDriver("/Users/corywheeler/Documents/projects/chromestuff/"))
+			{
+				// Go to the home page
+				driver.Navigate().GoToUrl("http://quasimatic.com");
+			}
+		}
+	}
+}
